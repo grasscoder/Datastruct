ea31711f8c23e8c5d21b65aff0074bc5c7f6f9f7
grasscoder <hu600hu@163.com>
2017-06-16 22:31:51 +0800

    6-16
    6-16

---
+++ src/BSTree.java
@@ -0,0 +1,22 @@
+
+public class BSTree {
+	class TreeNode{
+		private int val;
+		TreeNode parent;
+		TreeNode leftchild;
+		TreeNode rightchild;
+		public TreeNode(int val,TreeNode leftchild,TreeNode rightchild,TreeNode parent){
+			this.val = val;
+			this.leftchild = leftchild;
+			this.rightchild = rightchild;
+			this.parent = parent;
+		}//end constructor TreeNode
+	}//end class TreeNode
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
